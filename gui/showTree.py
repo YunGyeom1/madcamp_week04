5cab9486cda4970ddf5e1546625463f9321b3d2c
윤겸 <yungyeom@yungyeom-ui-MacBookAir.local>
2025-01-18 15:22:11 +0900

    Efe

--- gui/showTree.py
+++ gui/showTree.py
@@ -24,6 +24,21 @@ class TreeWidget(QWidget):
 
         # 트리 렌더링
         self.update_tree()
+    def place_node(self, node, x, y):
+        vnode = InteractiveNode(node, self.update_tree)
+        vnode.setPos(400 - 200 * node.height, y)  # x 간격 증가
+        self.scene.addItem(vnode)
+
+        dy = 0  # 세로 간격 감소
+        for ch in node.children:
+            child_x = 400 - 200 * ch.height  # x 간격 증가
+            child_y = y + dy  # y 간격 감소
+            self.place_node(ch, child_x, child_y)
+            self.add_edge(vnode.pos(), QPointF(child_x, child_y))
+            dy += ch.width * 100  # 하위 노드 간 간격 조정
+
+        vnode.node = node
+        node.vnode = vnode
 
     def add_edge(self, parent_pos, child_pos):
         parent = QPointF(parent_pos.x() + 75, parent_pos.y() + 15)  # x 간격 증가, y 간격 감소
@@ -53,51 +68,6 @@ class TreeWidget(QWidget):
         line2.setPen(QPen(Qt.red, 2))
         self.scene.addItem(line2)
     
-    def place_node(self, node, x, y):
-        vnode = InteractiveNode(node, self.update_tree)
-        vnode.setPos(400 - 200 * node.height, y)  # x 간격 증가
-        self.scene.addItem(vnode)
-
-        dy = 0  # 세로 간격 감소
-        for ch in node.children:
-            child_x = 400 - 200 * ch.height  # x 간격 증가
-            child_y = y + dy  # y 간격 감소
-            self.place_node(ch, child_x, child_y)
-            self.add_edge(vnode.pos(), QPointF(child_x, child_y))
-            dy += ch.width * 100  # 하위 노드 간 간격 조정
-
-        vnode.node = node
-        node.vnode = vnode
-    
     def update_tree(self):
         self.scene.clear()
         self.place_node(self.root, 100, 100)
-
-def create_sample_tree():
-    root = GoalNode("Root")
-    child1 = GoalNode("C1")
-    child2 = GoalNode("C2")
-    child3 = GoalNode("C3")
-    child4 = GoalNode("C4")
-    root.add_child(child1)
-    root.add_child(child2)
-    child1.add_child(child3)
-    child1.add_child(child4)
-    return root
-
-# 실행 코드
-if __name__ == "__main__":
-    import sys
-
-    app = QApplication(sys.argv)
-
-    # 샘플 트리 생성
-    root_node = create_sample_tree()
-
-    # TreeWidget 인스턴스 생성 및 테스트
-    tree_widget = TreeWidget(root_node)
-    tree_widget.setWindowTitle("TreeWidget Test")
-    tree_widget.setGeometry(100, 100, 800, 600)
-    tree_widget.show()
-
-    sys.exit(app.exec_())
